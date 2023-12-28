f2f6554aed06d5674c60a14dfd63e58f4f2f7a7a
Kryonn <kryojin@outlook.com>
2023-12-28 20:04:06 -0300

    28/12 - 20:04
    Alterações:
    -Adição do sistema de pause e GO

---
+++ Assets/scripts/pause.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pause : MonoBehaviour
+{
+    public bool on;
+    public bool go_pause;
+    player a;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1f;
+        on = false;
+        a = FindObjectOfType<player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        go_pause = a.go_pause;
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            pausar();
+        }
+        
+        if(go_pause == true)
+        {
+            go_pausar();
+        }
+    }
+
+    private void pausar()
+    {
+        if(on == false)
+        {
+            Time.timeScale = 0f;
+            on = true;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            on = false;
+        }
+    }
+
+    private void go_pausar()
+    {
+        Time.timeScale = 0f;
+        on = true;
+    }
+}
